b400a699ac9fc434bbeb5ba7eb955b4d2d3787a7
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-02-15 13:49:37 -0500

    start java branch

---
+++ src/Dashboard/Main.java
@@ -0,0 +1,11 @@
+package Dashboard;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+    
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String[]  args) {}
+
+}
