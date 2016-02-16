b2393418a025c92cff903a1a2b637b304ba6cba5
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-02-15 19:13:39 -0500

    add stream video player class

--- src/Dashboard/Main.java
+++ src/Dashboard/Main.java
@@ -1,11 +1,22 @@
 package Dashboard;
 
+import java.io.IOException;
+
 import javax.swing.JFrame;
+import Dashboard.VideoStreamPlayer;
 
 public class Main extends JFrame {
     
     private static final long serialVersionUID = 1L;
 
-    public static void main(String[]  args) {}
+    public static void main(String[]  args) {
+        VideoStreamPlayer vsp = new VideoStreamPlayer();
+        String[] arg = {"/Users/kelvinabrokwa/hacku/astute-dev.github.io/video/stock.mp4"};
+        try {
+            vsp.init(arg);
+        }
+        catch (InterruptedException e) { e.printStackTrace();}
+        catch (IOException e) { e.printStackTrace(); }
+    }
 
 }
